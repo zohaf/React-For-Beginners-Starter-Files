1b0d983390d944feea41dff44651a5724780d1ad
zohaf <zohafard@gmail.com>
2022-12-21 16:56:59 +0400

    counting orders and rendering them

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,13 +1,48 @@
 import { Component } from "react";
+import { formatPrice } from "./../helpers";
 
 class Order extends Component {
-  render() {
+  renderOrder = (key) => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>sorry {fish ? fish.name : "fish"} is not available!</li>
+      );
+    }
     return (
-      <div className="Order">
-        <p>order</p>
+      <li key={key}>
+        {count} lbs x {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      //we need fish that they are adding
+      const fish = this.props.fishes[key];
+      // we need the number of htat fish they are adding
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + fish.price * count;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
 }
 
 export default Order;
+// we want to show the name of each of the items
